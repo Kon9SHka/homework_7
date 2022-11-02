9e937785d4fea71e771c2c9dd919306875eac637
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-11-02 16:00:51 +0300

    added homework 7 task 1

---
+++ pr_1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int m = GetNumberFromUser("Введите m: ");
+int n = GetNumberFromUser("Введите n: ");
+
+double[,] array = GetArray(m,n);
+
+int GetNumberFromUser (string message) 
+{
+    while(true) 
+    {
+
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+        while(isCorrect)
+            return number;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+double [,] GetArray (int m, int n) 
+{
+    double[,] array = new double[m,n];
+    for (int i = 0; i < array.GetLength(0); i ++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j ++) 
+        {
+            array[i,j] = Math.Round(new Random().NextDouble()*100,2);
+            Console.Write($" {array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
